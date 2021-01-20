ab324f9f4096a8099b2e1af0fcd4a5630d8c4d38
Restioson <restiosondev@gmail.com>
2021-01-20 12:06:38 +0200

    load chunks where spawning is required

--- src/main/java/xyz/nucleoid/dungeons/dungeons/game/scripting/ScriptingUtil.java
+++ src/main/java/xyz/nucleoid/dungeons/dungeons/game/scripting/ScriptingUtil.java
@@ -1,6 +1,8 @@
 package xyz.nucleoid.dungeons.dungeons.game.scripting;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -47,5 +49,11 @@ public class ScriptingUtil {
         double y = min.getY();
 
         return new Vec3d(x, y, z);
+    }
+
+    public static void loadChunkAndSpawn(ServerWorld world, Entity entity, Vec3d pos) {
+        world.getChunk(new BlockPos(pos));
+        world.spawnEntity(entity);
+        entity.teleport(pos.x, pos.y, pos.z);
     }
 }
