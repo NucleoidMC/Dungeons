28699b3c89e37bb4ec4df43277ac9cc59d844393
redblueflame <contact@redblueflame.com>
2021-01-27 21:03:57 +0100

    Added testing command & looting

--- src/main/java/xyz/nucleoid/dungeons/dungeons/util/item/loot/DgArmorLoot.java
+++ src/main/java/xyz/nucleoid/dungeons/dungeons/util/item/loot/DgArmorLoot.java
@@ -1,15 +1,22 @@
 package xyz.nucleoid.dungeons.dungeons.util.item.loot;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import xyz.nucleoid.dungeons.dungeons.item.DgItems;
+import xyz.nucleoid.dungeons.dungeons.item.armor.DgArmorItem;
+import xyz.nucleoid.dungeons.dungeons.util.item.DgItemQuality;
+import xyz.nucleoid.dungeons.dungeons.util.item.material.DgArmorMaterial;
+import xyz.nucleoid.dungeons.dungeons.util.item.material.DgRangedWeaponWood;
 
 import java.util.Random;
 
 public class DgArmorLoot {
-
     public static ItemStack generate(Random random, double dungeonLevel) {
-        double meanLevel = dungeonLevel - 0.5;
-        return ItemStack.EMPTY;
+        DgArmorMaterial material = DgArmorMaterial.choose(random, dungeonLevel);
+        DgItemQuality quality = DgItemQuality.chooseInRange(random, material.minQuality, material.maxQuality);
+        // Choose a random piece of armor
+        EquipmentSlot slot = EquipmentSlot.fromTypeIndex(EquipmentSlot.Type.ARMOR, random.nextInt(4));
+        DgArmorItem item = DgItems.ARMOR_MANAGER.getArmor(slot, material);
+        return item != null ? item.createStack(quality) : ItemStack.EMPTY;
     }
-
-
 }
